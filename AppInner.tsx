b1ef31a789db23461d960b70b599b61ee779650e
hoin14 <zazahoin@gmail.com>
2024-10-06 08:30:37 +0900

    앱 실행시 토큰 있으면 자동로그인

--- AppInner.tsx
+++ AppInner.tsx
@@ -11,6 +11,13 @@ import SignUp from './src/pages/SignUp';
 import {RootState} from './src/store/reducer';
 import {useSelector} from 'react-redux';
 import useSocket from './src/hooks/useSocket';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import axios, {AxiosError} from 'axios';
+import Config from 'react-native-config';
+import userSlice from './src/slices/user';
+import {Alert} from 'react-native';
+import {useEffect} from 'react';
+import {useAppDispatch} from './src/store';
 
 export type LoggedInParamList = {
   Orders: undefined;
@@ -28,31 +35,69 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function AppInner() {
+  const dispatch = useAppDispatch();
   const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
   const [socket, disconnect] = useSocket();
 
-  React.useEffect(() => {
-    const heloCallback = (data: any) => {
+  useEffect(() => {
+    const callback = (data: any) => {
       console.log('hellow', data);
     };
     if (socket && isLoggedIn) {
       console.log(socket);
-      //socket.emit('login', 'hello');
-      socket.on('hello', heloCallback);
+      socket.emit('acceptOrder', 'hello');
+      socket.on('order', callback);
     }
     return () => {
       if (socket) {
-        socket.off('hello', heloCallback);
+        socket.off('order', callback);
       }
     };
   }, [isLoggedIn, socket]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (!isLoggedIn) {
       console.log('!isLoggedIn', !isLoggedIn);
       disconnect();
     }
   }, [isLoggedIn, disconnect]);
+
+  // 앱 실행 시 토큰 있으면 로그인하는 코드
+  useEffect(() => {
+    const getTokenAndRefresh = async () => {
+      try {
+        const token = await EncryptedStorage.getItem('refreshToken');
+        console.log(token);
+        if (!token) {
+          return;
+        }
+        const response = await axios.post(
+          `${Config.API_URL}/refreshToken`,
+          {},
+          {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        dispatch(
+          userSlice.actions.setUser({
+            name: response.data.data.name,
+            email: response.data.data.email,
+            accessToken: response.data.data.accessToken,
+          }),
+        );
+      } catch (error) {
+        console.error(error);
+        if ((error as AxiosError).response?.data.code === 'expired') {
+          Alert.alert('알림', '다시 로그인 해주세요.');
+        }
+      } finally {
+        // TODO: 스플래시 스크린 없애기
+      }
+    };
+    getTokenAndRefresh();
+  }, [dispatch]);
 
   return (
     <NavigationContainer>
